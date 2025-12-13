f2a1f426c5633679c46ed1e3c9d3935ec4d844a7
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-12-13 15:48:37 +0000

    chore: tweak entities

--- apps/api/instrumentation.ts
+++ apps/api/instrumentation.ts
@@ -1,5 +1,3 @@
-import { MediaItemState } from "@repo/database/entities/media-items/media-item.entity";
-import { Movie } from "@repo/database/entities/media-items/movie.entity";
 import "reflect-metadata";
 
 export async function register() {
