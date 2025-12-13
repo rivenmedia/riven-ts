0aaaeaf81e8a7da5f2ad77461ff13f4b45c0d41f
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-12-13 00:30:28 +0000

    chore: add yml to lint-staged prettier

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,3 +1,4 @@
 export default {
-    "*.{js,jsx,cjs,mjs,ts,tsx,cts,mts,json,md,mdx,html}": "prettier --write"
-}
+  "*.{js,jsx,cjs,mjs,ts,tsx,cts,mts,json,md,mdx,html,yml,yaml}":
+    "prettier --write",
+};
