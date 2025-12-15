43484958b92c0829d1fd220ba096a01a3a6594f4
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-12-15 00:13:21 +0000

    chore: add docker compose watch

--- apps/api/app/layout.tsx
+++ apps/api/app/layout.tsx
@@ -1,12 +1,8 @@
-import { logger } from "@repo/core-util-logger";
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  logger.info("Rendering RootLayout");
-
   return (
     <html lang="en">
       <body>{children}</body>
