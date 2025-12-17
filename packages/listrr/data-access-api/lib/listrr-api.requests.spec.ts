bf7adf6a980f13cc892aa129277f0fcbf0661836
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-12-17 01:14:53 +0000

    chore: add api key test

---
+++ packages/listrr/data-access-api/lib/listrr-api.requests.spec.ts
@@ -0,0 +1,24 @@
+import { ListrrAPI } from "./listrr-api.ts";
+import { it } from "@repo/core-util-vitest-config/test-context";
+import { http, HttpResponse } from "msw";
+import { expect } from "vitest";
+
+it('assigns the API to the "x-api-key" header', async ({ server }) => {
+  const listrrApi = new ListrrAPI("test-token");
+
+  server.use(
+    http.get("https://listrr.pro/test-endpoint", ({ request }) => {
+      if (request.headers.get("x-api-key") !== "Bearer test-token") {
+        return HttpResponse.error();
+      }
+
+      return HttpResponse.json({ success: true });
+    }),
+  );
+
+  const { parsedBody } = await listrrApi.fetch<{ success: boolean }>(
+    "/test-endpoint",
+  );
+
+  expect(parsedBody.success).toBe(true);
+});
