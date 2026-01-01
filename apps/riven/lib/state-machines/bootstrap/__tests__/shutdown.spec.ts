0f61d1362523cb706fe178e991857c5cc839ffcf
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-01-01 01:29:31 +0000

    chore: tidy shutdown tests

--- apps/riven/lib/state-machines/bootstrap/__tests__/shutdown.spec.ts
+++ apps/riven/lib/state-machines/bootstrap/__tests__/shutdown.spec.ts
@@ -2,16 +2,26 @@ import { it } from "./helpers/test-context.ts";
 import { waitFor } from "xstate";
 import { expect } from "vitest";
 
-it('transitions to "Shutdown" state on EXIT event', ({ actor }) => {
+it('transitions to "Shutdown" then "Exited" when shutting down', async ({
+  actor,
+}) => {
   actor.send({ type: "EXIT" });
 
   expect(actor.getSnapshot().value).toBe("Shutdown");
+
+  await waitFor(actor, (snapshot) => snapshot.matches("Exited"));
+
+  expect(actor.getSnapshot().value).toBe("Exited");
 });
 
-it(`emits the "riven.shutdown" event when entering the "Shutdown" state`, async ({
+it(`emits the "riven.shutdown" and "riven.exited" events when shutting down`, async ({
   actor,
 }) => {
-  actor.on("*", (event) => {
+  actor.on("riven.shutdown", (event) => {
+    expect(event.type).toBe("riven.shutdown");
+  });
+
+  actor.on("riven.exited", (event) => {
     expect(event.type).toBe("riven.exited");
   });
 
@@ -19,5 +29,5 @@ it(`emits the "riven.shutdown" event when entering the "Shutdown" state`, async 
 
   await waitFor(actor, (snapshot) => snapshot.matches("Exited"));
 
-  expect.assertions(1);
+  expect.assertions(2);
 });
