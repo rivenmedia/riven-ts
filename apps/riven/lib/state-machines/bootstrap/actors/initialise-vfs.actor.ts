a9aba9b4221dbaa908a9ee271369d0c959ef4964
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-01-19 19:52:55 +0000

    chore: add fuse configuration check

--- apps/riven/lib/state-machines/bootstrap/actors/initialise-vfs.actor.ts
+++ apps/riven/lib/state-machines/bootstrap/actors/initialise-vfs.actor.ts
@@ -1,7 +1,8 @@
+import Fuse from "fuse-native";
 import { fromPromise } from "xstate";
 import z from "zod";
 
-import { Fuse, ops } from "../../../vfs/index.ts";
+import { fuseOperations } from "../../../vfs/index.ts";
 
 export interface InitialiseVfsOutput {
   vfs: Fuse;
@@ -15,10 +16,29 @@ export const initialiseVfs = fromPromise<
   InitialiseVfsOutput,
   InitialiseVfsInput
 >(async ({ input: { mountPath } }) => {
-  const vfs = new Fuse(mountPath, ops, {
+  const isConfigured = await new Promise<boolean | undefined>(
+    (resolve, reject) => {
+      Fuse.isConfigured((err, val) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        }
+
+        resolve(val);
+      });
+    },
+  );
+
+  if (!isConfigured) {
+    throw new Error(
+      "FUSE is not configured on this system. Please run `cd apps/riven && sudo env PATH=$PATH npm run fuse:configure`.",
+    );
+  }
+
+  const vfs = new Fuse(mountPath, fuseOperations, {
     debug: z.stringbool().parse(process.env["VFS_DEBUG_LOGGING"]),
     autoUnmount: true,
-    allowOther: true,
+    // allowOther: true,
     force: true,
     mkdir: true,
     fsname: "riven_vfs",
--- apps/riven/lib/vfs/index.ts
+++ apps/riven/lib/vfs/index.ts
@@ -1,12 +1,9 @@
 import Fuse from "fuse-native";
 
-import "../types/fuse-native.d.ts";
 import { getattrSync } from "./operations/getattr.ts";
 import { readDirSync } from "./operations/readdir.ts";
 
-export const ops: Fuse.Operations = {
+export const fuseOperations: Fuse.Operations = {
   readdir: readDirSync,
   getattr: getattrSync,
 };
-
-export { Fuse };
