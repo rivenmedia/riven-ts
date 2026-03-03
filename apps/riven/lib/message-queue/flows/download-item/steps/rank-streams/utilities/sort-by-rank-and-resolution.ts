ac4c1c4f136613f5e611b01f1e29f849a1e8d7d2
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-03-03 19:37:23 +0000

    chore: prioritise rank in sorting

--- apps/riven/lib/message-queue/flows/download-item/steps/rank-streams/utilities/sort-by-rank-and-resolution.ts
+++ apps/riven/lib/message-queue/flows/download-item/steps/rank-streams/utilities/sort-by-rank-and-resolution.ts
@@ -5,15 +5,12 @@ import {
 } from "@repo/util-rank-torrent-name";
 
 export const sortByRankAndResolution = (a: RankedResult, b: RankedResult) => {
-  const { data: resA = "unknown" } = Resolution.safeParse(a.data.resolution);
+  if (a.rank !== b.rank) {
+    return b.rank - a.rank;
+  }
 
+  const { data: resA = "unknown" } = Resolution.safeParse(a.data.resolution);
   const { data: resB = "unknown" } = Resolution.safeParse(b.data.resolution);
 
-  // Combine rank and resolution into a single score for sorting
-  // This will prioritise higher ranks, even if it means a lower resolution,
-  // but will still prefer higher resolutions when ranks equal.
-  const combinedRankA = a.rank + ResolutionRank[resA];
-  const combinedRankB = b.rank + ResolutionRank[resB];
-
-  return combinedRankB - combinedRankA;
+  return ResolutionRank[resB] - ResolutionRank[resA];
 };
