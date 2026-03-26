8332faa6f49b1032402669f22b284f0129e4225c
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-03-26 00:37:12 +0000

    chore: add assertion

--- apps/riven/lib/database/seeders/shows/scraped-show.seeder.ts
+++ apps/riven/lib/database/seeders/shows/scraped-show.seeder.ts
@@ -1,6 +1,7 @@
 import { MediaItem, Stream } from "@repo/util-plugin-sdk/dto/entities";
 
 import { Seeder } from "@mikro-orm/seeder";
+import assert from "node:assert";
 
 import { StreamsSeeder } from "../streams/streams.seeder.ts";
 import { ShowSeeder } from "./show.seeder.ts";
@@ -11,10 +12,19 @@ export class ScrapedShowSeeder extends Seeder {
   async run(em: EntityManager) {
     await this.call(em, [ShowSeeder, StreamsSeeder]);
 
-    const show = await em.findOneOrFail(MediaItem, {
-      type: "show",
-    });
+    const show = await em.findOneOrFail(
+      MediaItem,
+      { type: "show" },
+      { orderBy: { createdAt: "desc" } },
+    );
 
     show.streams.set(await em.findAll(Stream));
+
+    await em.flush();
+
+    assert(
+      show.state === "scraped",
+      `Expected show state to be "scraped", got "${show.state}"`,
+    );
   }
 }
