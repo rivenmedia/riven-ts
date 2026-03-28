6856daadca1d75071287c9907ee5b63ccd0cc2e1
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-03-28 17:44:18 +0000

    fix: types

--- apps/riven/lib/state-machines/program/__tests__/helpers/test-context.ts
+++ apps/riven/lib/state-machines/program/__tests__/helpers/test-context.ts
@@ -5,38 +5,43 @@ import { vi } from "vitest";
 import { createActor, fromPromise } from "xstate";
 
 import { it as baseIt } from "../../../../__tests__/test-context.ts";
-import { bootstrapMachine } from "../../../bootstrap/index.ts";
+import {
+  type BootstrapMachineOutput,
+  bootstrapMachine,
+} from "../../../bootstrap/index.ts";
 import { rivenMachine } from "../../index.ts";
 
 import type { ValidPlugin } from "../../../../types/plugins.ts";
 
 export const it = baseIt
-
   .extend("input", () => ({
     sessionId: crypto.randomUUID(),
   }))
   .extend("machine", () => rivenMachine)
-  .extend("bootstrapMachineOutput", async ({}) => {
-    const testPlugin = await import("@repo/plugin-test");
+  .extend(
+    "bootstrapMachineOutput",
+    async ({}): Promise<BootstrapMachineOutput> => {
+      const testPlugin = await import("@repo/plugin-test");
 
-    return {
-      server: {} as never,
-      plugins: new Map<symbol, ValidPlugin>([
-        [
-          testPlugin.default.name,
-          {
-            config: testPlugin.default,
-            dataSources: new DataSourceMap(),
-            status: "valid",
-          },
-        ],
-      ]),
-      pluginQueues: new Map(),
-      pluginWorkers: new Map(),
-      publishableEvents: new Set(),
-      vfs: {} as never,
-    };
-  })
+      return {
+        server: {} as never,
+        plugins: new Map<symbol, ValidPlugin>([
+          [
+            testPlugin.default.name,
+            {
+              config: testPlugin.default,
+              dataSources: new DataSourceMap(),
+              status: "valid",
+            },
+          ],
+        ]),
+        pluginQueues: new Map(),
+        pluginWorkers: new Map(),
+        publishableEvents: new Set(),
+        vfs: {} as never,
+      };
+    },
+  )
   .extend("bootstrapMachineActorLogic", bootstrapMachine)
   .extend("stopGqlServerActorLogic", fromPromise(vi.fn()))
   .extend("actor", ({ machine, input }, { onCleanup }) => {
