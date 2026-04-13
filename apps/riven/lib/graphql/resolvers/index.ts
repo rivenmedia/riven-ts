c9fd1d342c06a7ddd1474ec1fa7455c1d365beea
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-04-13 07:38:44 +0100

    chore: move resolvers

--- apps/riven/lib/graphql/resolvers/index.ts
+++ apps/riven/lib/graphql/resolvers/index.ts
@@ -1,10 +1,10 @@
-import { EpisodeResolver } from "./episode.resolver.ts";
+import { EpisodeResolver } from "./episode/episode.resolver.ts";
 import { ItemRequestResolver } from "./item-request/item-request.resolver.ts";
 import { MediaEntryResolver } from "./media-entry/media-entry.resolver.ts";
-import { MediaItemResolver } from "./media-item.resolver.ts";
-import { MovieResolver } from "./movie.resolver.ts";
-import { SeasonResolver } from "./season.resolver.ts";
-import { ShowResolver } from "./show.resolver.ts";
+import { MediaItemResolver } from "./media-item/media-item.resolver.ts";
+import { MovieResolver } from "./movie/movie.resolver.ts";
+import { SeasonResolver } from "./season/season.resolver.ts";
+import { ShowResolver } from "./show/show.resolver.ts";
 import { VfsResolver } from "./vfs/vfs.resolver.ts";
 
 export const resolvers = [
