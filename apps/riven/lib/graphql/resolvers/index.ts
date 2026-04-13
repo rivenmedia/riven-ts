71a3ccdfe75339a2f3571cb0c9c885c590a63de6
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-04-13 07:37:29 +0100

    chore: fix path

--- apps/riven/lib/graphql/resolvers/index.ts
+++ apps/riven/lib/graphql/resolvers/index.ts
@@ -1,6 +1,6 @@
-import { MediaEntryResolver } from "../media-entry/media-entry.resolver.ts";
 import { EpisodeResolver } from "./episode.resolver.ts";
 import { ItemRequestResolver } from "./item-request/item-request.resolver.ts";
+import { MediaEntryResolver } from "./media-entry/media-entry.resolver.ts";
 import { MediaItemResolver } from "./media-item.resolver.ts";
 import { MovieResolver } from "./movie.resolver.ts";
 import { SeasonResolver } from "./season.resolver.ts";
