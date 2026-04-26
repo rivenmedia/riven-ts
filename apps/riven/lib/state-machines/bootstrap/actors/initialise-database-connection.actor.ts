80f9699e081bbce74c27d1688dd5dde254840ed5
Bryan Lee <me@bryanl.ee>
2026-04-26 13:34:24 -0700

    feat(database): omit options if ssl is not present

--- apps/riven/lib/state-machines/bootstrap/actors/initialise-database-connection.actor.ts
+++ apps/riven/lib/state-machines/bootstrap/actors/initialise-database-connection.actor.ts
@@ -7,27 +7,44 @@ import { initORM } from "../../../database/database.ts";
 import { logger } from "../../../utilities/logger/logger.ts";
 import { settings } from "../../../utilities/settings.ts";
 
+function createDatabaseSslOptions() {
+  const ca = settings.databaseSslRootCert
+    ? readFileSync(settings.databaseSslRootCert, "utf8")
+    : undefined;
+  const cert = settings.databaseSslCert
+    ? readFileSync(settings.databaseSslCert, "utf8")
+    : undefined;
+  const key = settings.databaseSslKey
+    ? readFileSync(settings.databaseSslKey, "utf8")
+    : undefined;
+
+  if (!ca && !cert && !key) {
+    return undefined;
+  }
+
+  return {
+    ...(ca && { ca }),
+    ...(cert && { cert }),
+    ...(key && { key }),
+  };
+}
+
 export const initialiseDatabaseConnection = fromPromise(async () => {
   const databaseConfig = await createDatabaseConfig({
     logger,
   });
+
+  const sslOptions = createDatabaseSslOptions();
+
   const { database } = await initORM({
     ...databaseConfig,
     clientUrl: settings.databaseUrl,
     debug: settings.databaseDebugLogging,
-    driverOptions: {
-      ssl: {
-        ca: settings.databaseSslRootCert
-          ? readFileSync(settings.databaseSslRootCert)
-          : undefined,
-        cert: settings.databaseSslCert
-          ? readFileSync(settings.databaseSslCert)
-          : undefined,
-        key: settings.databaseSslKey
-          ? readFileSync(settings.databaseSslKey)
-          : undefined,
+    ...(sslOptions && {
+      driverOptions: {
+        ssl: sslOptions,
       },
-    },
+    }),
     extensions: [Migrator],
   });
 
