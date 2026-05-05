8238cb8239edaa3d6a7c3281ef0f95b6a160993f
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-05-05 21:56:19 +0100

    chore: delete unused flow

--- apps/riven/lib/message-queue/flows/request-item/enqueue-request-item.spec.ts
+++
@@ -1,7 +0,0 @@
-import { it } from "vitest";
-
-it.todo(
-  "creates a child job with the correct data for each content service plugin",
-);
-
-it.todo("creates a root job with the correct data");
--- apps/riven/lib/message-queue/flows/request-item/request-item.processor.ts
+++
@@ -1,9 +0,0 @@
-import { requestItemProcessorSchema } from "./request-item.schema.ts";
-
-export const requestItemProcessor = requestItemProcessorSchema.implementAsync(
-  async ({ job, signal }, { sendEvent, services: { itemRequestService } }) => {
-    const { item } = job.data;
-
-    console.log(item);
-  },
-);
