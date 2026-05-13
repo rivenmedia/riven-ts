0ebc5a2cdf3f1e9cbd41e3650890129504966ee2
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-05-13 16:31:41 +0100

    fix: self-signed certificate errors

--- apps/riven/lib/vfs/utilities/requests/request-agent.ts
+++ apps/riven/lib/vfs/utilities/requests/request-agent.ts
@@ -6,6 +6,7 @@ const requestAgent = new Agent({
   keepAliveMaxTimeout: config.activityTimeoutSeconds * 1000,
   connect: {
     timeout: config.connectTimeoutSeconds * 1000,
+    rejectUnauthorized: false,
   },
 }).compose(interceptors.retry(), interceptors.redirect());
 
