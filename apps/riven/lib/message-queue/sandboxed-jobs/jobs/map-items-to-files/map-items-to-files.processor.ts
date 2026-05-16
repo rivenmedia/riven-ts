42222dee9fe95f7fad686ec4e1c04978bc9f8c45
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-05-16 20:54:16 +0100

    chore: ensure mapItemsToFiles is in an async context

--- apps/riven/lib/message-queue/sandboxed-jobs/jobs/map-items-to-files/map-items-to-files.processor.ts
+++ apps/riven/lib/message-queue/sandboxed-jobs/jobs/map-items-to-files/map-items-to-files.processor.ts
@@ -7,7 +7,7 @@ import { mapItemsToFiles } from "./utilities/map-items-to-files.ts";
 
 export default createSandboxedJobProcessor(
   MapItemsToFilesSandboxedJob,
-  mapItemsToFilesProcessorSchema.implementAsync(function ({ job }) {
-    return mapItemsToFiles(job.data.files);
+  mapItemsToFilesProcessorSchema.implementAsync(async function ({ job }) {
+    return Promise.resolve(mapItemsToFiles(job.data.files));
   }),
 );
