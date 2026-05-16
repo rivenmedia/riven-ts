c454c9c6b67ece25d626e6ea793f024f346fe8c1
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-05-16 15:04:58 +0100

    chore: remove knip ignore

--- knip.config.ts
+++ knip.config.ts
@@ -45,7 +45,6 @@ export default {
     ".": {
       entry: [".husky/install.mjs", "turbo/generators/config.ts!"],
       project: ["turbo/**/*.ts"],
-      ignoreDependencies: ["node"],
       ignoreBinaries: ["tail"],
     },
     "apps/riven": {
