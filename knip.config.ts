2427c62a5d7b4217177ea6bf9f8196ae6ef46e88
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-05-16 14:02:39 +0100

    chore: omit node and tail from knip errors

--- knip.config.ts
+++ knip.config.ts
@@ -45,6 +45,8 @@ export default {
     ".": {
       entry: [".husky/install.mjs", "turbo/generators/config.ts!"],
       project: ["turbo/**/*.ts"],
+      ignoreDependencies: ["node"],
+      ignoreBinaries: ["tail"],
     },
     "apps/riven": {
       entry: [...defaultEntry],
