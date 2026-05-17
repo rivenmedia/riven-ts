b9a4fb9e5604e3bb13b54bcaa25fb86f85e71efc
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-05-18 00:31:16 +0100

    chore: add tests

--- apps/riven/lib/utilities/logger/formatters/mask.format.ts
+++ apps/riven/lib/utilities/logger/formatters/mask.format.ts
@@ -1,26 +1,23 @@
 import { format } from "winston";
 
+const apiKeyPattern = /(api_?)?(token|key)[=:](\w|-)+/gi;
+const userDirectoryPattern = /\/(home|Users)\/\w+\//g;
+
 function maskMessage(message: unknown) {
   if (typeof message !== "string") {
     return message;
   }
 
-  const apiKeyPattern = /(api_?)?(token|key)[=:](\w|-)+/gi;
-  const hasSensitiveData = apiKeyPattern.test(message);
+  const hasSensitiveData =
+    apiKeyPattern.test(message) || userDirectoryPattern.test(message);
 
   if (!hasSensitiveData) {
     return message;
   }
 
-  return message.replaceAll(apiKeyPattern, (match) => {
-    const [prefix] = match.split(/[:=]/);
-
-    if (!prefix) {
-      return match;
-    }
-
-    return `${prefix}=[REDACTED]`;
-  });
+  return message
+    .replaceAll(apiKeyPattern, "$1$2=[REDACTED]")
+    .replaceAll(userDirectoryPattern, "/$1/[REDACTED]/");
 }
 
 export const maskFormat = format((info) => {
