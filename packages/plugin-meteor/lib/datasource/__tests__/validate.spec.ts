e3899658db617ed53b2ba99ad9a934c989d5fbca
davidemarcoli <davide@marcoli.ch>
2026-05-18 13:19:50 +0200

    feat(meteor): add new plugin for meteor scraper

---
+++ packages/plugin-meteor/lib/datasource/__tests__/validate.spec.ts
@@ -0,0 +1,33 @@
+import { HttpResponse, http } from "msw";
+import { expect } from "vitest";
+
+import { it } from "../../__tests__/meteor.test-context.ts";
+import { pluginConfig } from "../../meteor-plugin.config.ts";
+import { MeteorAPI } from "../meteor.datasource.ts";
+
+it("returns false if the request fails", async ({ server, dataSourceMap }) => {
+  server.use(
+    http.get("**/validate", () =>
+      HttpResponse.json({ success: false }, { status: 401 }),
+    ),
+  );
+
+  const meteorApi = dataSourceMap.get(MeteorAPI);
+  const isValid = await meteorApi.validate();
+
+  expect(isValid).toBe(false);
+});
+
+it("returns true if the request succeeds", async ({
+  server,
+  dataSourceMap,
+}) => {
+  server.use(
+    http.get("**/validate", () => HttpResponse.json({ success: true })),
+  );
+
+  const meteorApi = dataSourceMap.get(MeteorAPI);
+  const isValid = await meteorApi.validate();
+
+  expect(isValid).toBe(true);
+});
---
+++ packages/plugin-meteor/lib/schema/__tests__/meteor.resolver.spec.ts
@@ -0,0 +1,33 @@
+import { HttpResponse, http } from "msw";
+import assert from "node:assert";
+import { expect } from "vitest";
+
+import { it } from "../../__tests__/meteor.test-context.ts";
+import { MeteorAPI } from "../../datasource/meteor.datasource.ts";
+import { pluginConfig } from "../../meteor-plugin.config.ts";
+
+it('returns the validation status when calling "meteorIsValid" query', async ({
+  gqlContext,
+  gqlServer,
+  server,
+}) => {
+  server.use(
+    http.get("**/validate", () => HttpResponse.json({ success: true })),
+  );
+
+  const { body } = await gqlServer.executeOperation(
+    {
+      query: `
+        query MeteorIsValid {
+          meteorIsValid
+        }
+      `,
+    },
+    { contextValue: gqlContext },
+  );
+
+  assert(body.kind === "single");
+
+  expect(body.singleResult.errors).toBeUndefined();
+  expect(body.singleResult.data?.["meteorIsValid"]).toBe(true);
+});
