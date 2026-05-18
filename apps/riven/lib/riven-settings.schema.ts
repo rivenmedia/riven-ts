468770e33b82525c173027f44a11b3352f35b49b
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-05-18 19:18:06 +0100

    chore: remove unused export

--- apps/riven/lib/riven-settings.schema.ts
+++ apps/riven/lib/riven-settings.schema.ts
@@ -7,8 +7,6 @@ import z from "zod";
 import { CorePlugins } from "./schemas/core-plugins.schema.ts";
 import { LogLevel } from "./utilities/logger/log-levels.ts";
 
-export const unused = true;
-
 export const RivenSettings = z.object({
   attemptUnknownDownloads: z
     .stringbool()
