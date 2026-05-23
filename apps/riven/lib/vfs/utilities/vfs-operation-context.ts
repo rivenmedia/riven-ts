063d3500e6622c728de19110ff2b23fbe1b99625
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-05-23 12:54:12 +0100

    chore(core): add VFS operation async context (#173)

---
+++ apps/riven/lib/vfs/utilities/vfs-operation-context.ts
@@ -0,0 +1,56 @@
+import assert from "node:assert";
+import { AsyncLocalStorage } from "node:async_hooks";
+import { Buffer } from "node:buffer";
+
+import type { Promisable } from "type-fest";
+
+export type VfsOperationContext = (
+  | {
+      operationName: "open";
+      flags: number;
+    }
+  | {
+      operationName: "release";
+      fd: number;
+    }
+  | {
+      operationName: "read";
+      fd: number;
+      position: number;
+      length: number;
+      buffer: Buffer;
+    }
+  | {
+      operationName: "getattr" | "readdir";
+    }
+) & {
+  path: string;
+};
+
+const vfsOperationContext = new AsyncLocalStorage<VfsOperationContext>();
+
+export function withVfsOperationContext<T extends () => Promisable<unknown>>(
+  context: VfsOperationContext,
+  callback: T,
+) {
+  return vfsOperationContext.run<unknown>(context, callback) as ReturnType<T>;
+}
+
+export function getVfsOperationContext<
+  T extends VfsOperationContext["operationName"],
+>(operationName?: T): Extract<VfsOperationContext, { operationName: T }> {
+  const context = vfsOperationContext.getStore();
+
+  if (!context) {
+    throw new Error("No VFS operation context available");
+  }
+
+  if (operationName) {
+    assert(
+      context.operationName === operationName,
+      `VFS operation context mismatch: expected ${operationName} but got ${context.operationName}`,
+    );
+  }
+
+  return context as Extract<VfsOperationContext, { operationName: T }>;
+}
