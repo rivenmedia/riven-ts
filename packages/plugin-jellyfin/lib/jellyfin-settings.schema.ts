2593e58e0c8896f660e1a82c5612958cad0f6430
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-05-30 20:45:08 +0100

    chore: fix url schema

--- packages/plugin-jellyfin/lib/jellyfin-settings.schema.ts
+++ packages/plugin-jellyfin/lib/jellyfin-settings.schema.ts
@@ -3,14 +3,17 @@ import z from "zod";
 export const JellyfinSettings = z.object({
   jellyfinToken: z
     .string()
-    .min(1, "Jellyfin Token is required")
-    .describe("Jellyfin Token for accessing the Jellyfin API."),
+    .min(1, "Jellyfin token is required")
+    .describe("Jellyfin token for accessing the Jellyfin API."),
   jellyfinServerUrl: z
-    .httpUrl("Jellyfin Server URL must be a valid URL")
+    .url({
+      error: "Jellyfin server URL must be a valid URL",
+      protocol: z.regexes.httpProtocol,
+    })
     .describe("The URL of your Jellyfin server, e.g., http://localhost:8096/"),
   jellyfinLibraryPath: z
     .string()
-    .min(1, "Jellyfin Library Path is required")
+    .min(1, "Jellyfin library path is required")
     .describe(
       'The start of Jellyfin library paths, e.g. "/mount" in "/mount/movies"',
     )
