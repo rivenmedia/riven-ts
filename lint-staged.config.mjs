e1c01a290a4dda9755c9de634adc1a8235b38ab2
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-06-20 12:37:03 +0100

    chore: add svelte to lint-staged prettier hook

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,7 +2,7 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  "*.{js,jsx,cjs,mjs,ts,tsx,cts,mts,json,md,mdx,html,yml,yaml}":
+  "*.{js,jsx,cjs,mjs,ts,tsx,cts,mts,json,md,mdx,html,yml,yaml,svelte}":
     "prettier --write",
   "package.json": "sort-package-json",
   "*-settings.schema.ts": async () => "turbo codegen:config-docs",
