3a884de4e76c7a82d75158d2f7970058175a2b31
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-06-23 12:41:55 +0100

    chore: fix houdini vite plugin causing vitest to hang

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -9,16 +9,27 @@ import { type UserConfig, defineConfig, loadEnv, mergeConfig } from "vite";
 import packageJson from "./package.json" with { type: "json" };
 import svelteConfig from "./svelte.config.ts";
 
-export default defineConfig((config) => {
+export default defineConfig(async (config) => {
   const env = loadEnv(config.mode, process.cwd(), "");
   const backendUrl = env["BACKEND_URL"] ?? "http://localhost:8080";
   const apiKey = env["BACKEND_API_KEY"] ?? env["RIVEN_SETTING__API_KEY"] ?? "";
 
   const baseConfig = baseVitestConfig(config);
 
+  const houdiniPlugin = (await houdini()).filter((option) => {
+    if (config.mode !== "test") {
+      return true;
+    }
+
+    // Remove any hot update listeners that cause Vitest to hang
+    return (
+      typeof option === "object" && option != null && !("hotUpdate" in option)
+    );
+  });
+
   return mergeConfig<UserConfig, UserConfig>(baseConfig, {
     plugins: [
-      houdini(),
+      houdiniPlugin,
       tailwindcss(),
       sveltekit(svelteConfig.kit),
       svelteTesting(),
