432e2d16baea23fe54451986668693e747d28924
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-08-23 21:15:29 +0100

    fix: vitest node_modules test inclusion

--- packages/core/util-vitest-config/lib/base.ts
+++ packages/core/util-vitest-config/lib/base.ts
@@ -1,33 +1,36 @@
 import { fileURLToPath } from "node:url";
 import swc from "unplugin-swc";
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig, mergeConfig } from "vitest/config";
 
 export const baseVitestConfig = defineConfig(() => {
   const isWatch = process.argv.includes("--watch");
   const ignorePatterns = ["**/{__generated__,docker-data,.next,.turbo}/**"];
 
-  return defineConfig({
-    test: {
-      globals: true, // Enables testing-library auto cleanup
-      exclude: ignorePatterns,
-      restoreMocks: true,
-      coverage: {
-        enabled: !isWatch,
-        exclude: [...ignorePatterns, "**/__tests__/**"],
+  return mergeConfig(
+    { test: configDefaults },
+    {
+      test: {
+        globals: true, // Enables testing-library auto cleanup
+        exclude: ignorePatterns,
+        restoreMocks: true,
+        coverage: {
+          enabled: !isWatch,
+          exclude: [...ignorePatterns, "**/__tests__/**"],
+        },
+        setupFiles: [
+          fileURLToPath(
+            import.meta.resolve("./setup-files/restore-environment.ts"),
+          ),
+        ],
+        retry: process.env["CI"] ? 2 : 0,
+        hookTimeout: 30_000,
       },
-      setupFiles: [
-        fileURLToPath(
-          import.meta.resolve("./setup-files/restore-environment.ts"),
-        ),
-      ],
-      retry: process.env["CI"] ? 2 : 0,
-      hookTimeout: 30_000,
-    },
-    plugins: [swc.vite()],
-    server: {
-      watch: {
-        ignored: ignorePatterns,
+      plugins: [swc.vite()],
+      server: {
+        watch: {
+          ignored: ignorePatterns,
+        },
       },
     },
-  });
+  );
 });
