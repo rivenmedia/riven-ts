ba9aaeb680b09ddf98f41e343f65e0cb9e3f3c8f
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-08-24 20:09:06 +0100

    feat: add page shell component

---
+++ apps/frontend/components/page-shell/page-shell.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+
+import type { HTMLAttributes } from "react";
+
+export function PageShell({
+  className,
+  children,
+  ...restProps
+}: HTMLAttributes<HTMLElement>) {
+  return (
+    <main
+      className={cn(
+        "mt-4 flex flex-col gap-6 p-4 pb-24 md:mt-14 md:gap-8 md:p-8 md:px-16 animate-in fade-in duration-600 slide-in-from-bottom-5 ease-[easeOutCubic]",
+        className,
+      )}
+      {...restProps}
+    >
+      {children}
+    </main>
+  );
+}
