c662e460aa64fd6b32054b4b8e3e534a04bfc722
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-09-24 22:54:40 +0100

    chore: delete core changes

--- apps/riven/lib/database/entities/index.ts
+++
@@ -1,5 +0,0 @@
-export { User } from "./user.entity.ts";
-export { Account } from "./account.entity.ts";
-export { Passkey } from "./passkey.entity.ts";
-export { Session } from "./session.entity.ts";
-export { Verification } from "./verification.entity.ts";
